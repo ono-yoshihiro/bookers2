af46533c8b263d65749cb664259fcc83bdb13447
EC2 Default User <ec2-user@ip-172-31-11-44.ap-northeast-1.compute.internal>
2022-08-13 19:13:18 +0000

    [WIP] Add codes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+    def after_sign_in_path_for(resource)
+      about_path #ユーザー（自分）の詳細ページへ
+    end
+
+    ##サインアウト後はデフォルト設定どおりルートパスに遷移させるためafter_sign_out_path_forメソッドは記述しない。
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    end
+
 end
